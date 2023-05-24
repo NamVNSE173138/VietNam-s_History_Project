9b44fc89b50580c1ca9335d8465b0ed1e1120bbe
Dao <daoltse173161@fpt.edu.vn>
2023-05-24 20:37:08 +0700

    Post layout 01

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import SweetPagination from "sweetpagination";
+import "./Post.css";
 
 function Items() {
   const [currentPageData, setCurrentPageData] = useState(new Array(2).fill());
@@ -8,9 +9,23 @@ function Items() {
   return (
     <div>
       {currentPageData.map((item) => (
-        <div>
-          <h3>Item #{item}</h3>
-        </div>
+        <div className="post-container">
+          <h3>This is post #{item}</h3>
+            <div className="post">
+              <div class="post-info">
+                <img src="https://cartoonavatar.com/wp-content/uploads/2022/01/Business-Avatar-On-Circle-Background.png" alt="" className="post-author-avatar"/>
+                  <h4 className="post-author-name">Author's name</h4>
+                  <p className="time">May 21st, 08:50 AM</p>
+              </div>
+              <div className="post-content">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ad delectus ullam dolorum. Aut sint ipsum, voluptatum accusamus vitae beatae nihil tempora, error, nisi sapiente ducimus nesciunt quasi eveniet dicta.</p>
+              </div>
+                <div className="post-action">
+                    <button className="comment-button">Comment</button>
+                    <button className="report-button">Report</button>
+              </div>
+            </div>
+          </div>
       ))}
 
       <SweetPagination
