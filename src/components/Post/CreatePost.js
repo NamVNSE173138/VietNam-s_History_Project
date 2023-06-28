05154e5a25d27d57dcc6e402b4ce152af1e55982
deadline-backup <132864708+deadline-backup@users.noreply.github.com>
2023-06-28 20:30:18 +0700

    Add files via upload

---
+++ src/components/Post/CreatePost.js
@@ -0,0 +1,340 @@
+// import { PlusOutlined } from "@ant-design/icons";
+// import {
+//   Button,
+//   Cascader,
+//   Checkbox,
+//   DatePicker,
+//   Form,
+//   Input,
+//   InputNumber,
+//   Radio,
+//   Select,
+//   Slider,
+//   Switch,
+//   TreeSelect,
+//   Upload,
+//   Modal,
+// } from "antd";
+// import { useState } from "react";
+// import { Link, useNavigate } from "react-router-dom";
+// import axios from "axios";
+
+
+// const { RangePicker } = DatePicker;
+// const { TextArea } = Input;
+
+// const normFile = (e) => {
+//   if (Array.isArray(e)) {
+//     return e;
+//   }
+//   return e?.fileList;
+// };
+
+// const tailFormItemLayout = {
+//   wrapperCol: {
+//     xs: {
+//       span: 24,
+//       offset: 0,
+//     },
+//     sm: {
+//       span: 16,
+//       offset: 8,
+//     },
+//   },
+// };
+
+// const FormDisabledDemo = () => {
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [description, setDescription] = useState("");
+//   const [modal2Open, setModal2Open] = useState(false);
+//   const navigate = useNavigate();
+
+//   const showModal = () => {
+//     setIsModalOpen(true);
+//   };
+
+//   const handleOk = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   const handleCancel = () => {
+//     setIsModalOpen(false);
+//   };
+
+//   const onFinish = async (values) => {
+//     const session = JSON.parse(sessionStorage.getItem("session"));
+//     const userId = session?.userID;
+
+//     if (!userId) {
+//       // Handle session not found error
+//       return;
+//     }
+
+//     const newPost = {
+//       ...values,
+//       authorID: userId,
+//       description: description,
+//       like: 0,
+//       reportCounter: 0,
+//       commentCounter: 0,
+//       commentID: null,
+//       createAt: 123123123,
+//       id: 0,
+//     };
+
+//     try {
+//       const response = await axios.post(
+//         "https://64890c550e2469c038fe9625.mockapi.io/VN_HS/post",
+//         newPost
+//       );
+//       console.log("Response data:", response.data);
+//       Modal.success({
+//         title: "Post created successfully",
+//         onOk: () => {
+//           navigate("/posts");
+//         },
+//       });
+//     } catch (error) {
+//       console.error("Error creating post:", error);
+//       Modal.error({
+//         title: "Failed to create post",
+//       });
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="post">
+//         <Form
+//           labelCol={{
+//             span: 4,
+//           }}
+//           wrapperCol={{
+//             span: 14,
+//           }}
+//           layout="horizontal"
+//           style={{
+//             maxWidth: 600,
+//           }}
+//           onFinish={onFinish}
+//         >
+//         <Form.Item label="TextArea" name="textarea">
+//         <TextArea rows={4} onChange={(e) => setDescription(e.target.value)} />
+//         </Form.Item>
+
+//           <Form.Item
+//             name="agreement"
+//             valuePropName="checked"
+//             rules={[
+//               {
+//                 validator: (_, value) =>
+//                   value
+//                     ? Promise.resolve()
+//                     : Promise.reject(new Error("Please accept the agreement")),
+//               },
+//             ]}
+//             {...tailFormItemLayout}
+//           >
+//             <Checkbox>
+//               I have read the{" "}
+//               <Link onClick={showModal}>agreement</Link>
+//             </Checkbox>
+//           </Form.Item>
+
+//           <Modal
+//             title="User Agreement"
+//             visible={modal2Open}
+//             onOk={handleOk}
+//             onCancel={handleCancel}
+//           >
+//             <p>This is the user agreement.</p>
+//             <p>
+//               By clicking "I have read the agreement", you agree to the terms.
+//             </p>
+//           </Modal>
+
+//           <Form.Item {...tailFormItemLayout}>
+//             <Button type="primary" htmlType="submit">
+//               Register
+//             </Button>
+//           </Form.Item>
+//         </Form>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default FormDisabledDemo;
+
+
+import { PlusOutlined } from "@ant-design/icons";
+import {
+  Button,
+  Cascader,
+  Checkbox,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Radio,
+  Select,
+  Slider,
+  Switch,
+  TreeSelect,
+  Upload,
+  Modal,
+} from "antd";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const FormDisabledDemo = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modal2Open, setModal2Open] = useState(false);
+  const [description, setDescription] = useState("");
+  const storedSession = JSON.parse(sessionStorage.getItem("session"));
+  const navigate = useNavigate();
+
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  // console.log(localStorage.getItem(username));
+  const onFinish = async (values) => {
+    const {
+      authorID,
+      like,
+      description,
+      reportCounter,
+      commentCounter,
+      commentID,
+      createAt,
+      id,
+    } = values;
+
+    const newPost = {
+      authorID: storedSession.username,
+      like: 0,
+      description,
+      reportCounter: 0,
+      commentCounter: 0,
+      commentID: null,
+      createAt: 123123123,
+      id: 0,
+    };
+
+    try {
+      const response = await axios.post(
+        "https://64890c550e2469c038fe9625.mockapi.io/VN_HS/post",
+        newPost
+      );
+      console.log("Post created successfully:", response.data);
+      Modal.success({
+        title: "User created successfully",
+        // content: `The user "${username}" was created successfully!`,
+        onOk: () => {
+          navigate("/posts");
+        },
+      });
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="post">
+        <Form
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 14,
+          }}
+          layout="horizontal"
+          style={{
+            maxWidth: 600,
+          }}
+          onFinish={onFinish}
+        >
+         <Form.Item
+          name="description"
+          label="Username"
+         
+        >
+          <Input />
+        </Form.Item>
+
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value
+                    ? Promise.resolve()
+                    : Promise.reject(new Error("Please accept the agreement")),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              I have read the{" "}
+              <Link onClick={() => setModal2Open(true)}>agreement</Link>
+            </Checkbox>
+          </Form.Item>
+
+          <Modal
+            title="User Agreement"
+            visible={modal2Open}
+            onOk={() => setModal2Open(false)}
+            onCancel={() => setModal2Open(false)}
+          >
+            <p>This is the user agreement.</p>
+            <p>
+              By clicking "I have read the agreement", you agree to the terms.
+            </p>
+          </Modal>
+
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </>
+  );
+};
+
+export default FormDisabledDemo;
