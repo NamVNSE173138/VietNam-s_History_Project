de6f767c8146c9636543d81d519655543c03df5a
deadline-backup <132864708+deadline-backup@users.noreply.github.com>
2023-07-05 21:12:20 +0700

    Update EventDetail.js

--- src/components/Event/EventDetail.js
+++ src/components/Event/EventDetail.js
@@ -1,31 +1,4 @@
-// import React from "react";
 
-// const EventDetailPage = () => {
-//   const event = {
-//     title: "Event Title",
-//     time: "December 22, 1999",
-//     images: [
-//       "https://i.pinimg.com/564x/8e/81/53/8e8153f8bd9eecfc9939f49202742464.jpg",
-//       "https://i.pinimg.com/564x/8e/81/53/8e8153f8bd9eecfc9939f49202742464.jpg",
-//     ],
-//     description: "Event description ở đây nha",
-//   };
-
-//   return (
-//     <div>
-//       <h1>{event.title}</h1>
-//       <p>Time: {event.time}</p>
-//       <div>
-//         {event.images.map((image, index) => (
-//           <img key={index} src={image} alt={`Event Image ${index}`} />
-//         ))}
-//       </div>
-//       <p>{event.description}</p>
-//     </div>
-//   );
-// };
-
-// export default EventDetailPage;
 
 import axios from "axios";
 import { useParams } from "react-router-dom"; // Import the useParams hook from react-router-dom
@@ -76,17 +49,17 @@ export default EventDetail;
 // import axios from "axios";
 // import { useParams } from "react-router-dom";
 // import { useState, useEffect } from "react";
-// import { Avatar, List, Space, Button, message } from "antd";
+// import { Avatar, List, Space, Button, message, Modal } from "antd";
 // import { LikeOutlined, MessageOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
 // import { Popconfirm } from "antd";
 // import CreatePost from "../../pages/CreatePost";
+// import EventContext from "./EventContext";
 
 // const EventDetail = () => {
 //   const { eventID } = useParams();
 //   const [event, setEvent] = useState(null);
 //   const [posts, setPosts] = useState([]);
 //   const [isReported, setIsReported] = useState(false);
-
 
 //   const PostItem = ({ post }) => (
 //     <List.Item
@@ -114,18 +87,17 @@ export default EventDetail;
 //           </Space>
 //         </Button>,
 //         <Popconfirm
-//         title="Report"
-//         description="Are you sure to report this post?"
-//         onConfirm={handleReportConfirm}
-//         onCancel={handleReportCancel}
-//         okText="Yes"
-//         cancelText="No"
-//       >
-//         <Button type="default">
-//           <ExclamationCircleOutlined /> Report
-//         </Button>
-//       </Popconfirm>
-//       ,
+//           title="Report"
+//           description="Are you sure to report this post?"
+//           onConfirm={handleReportConfirm}
+//           onCancel={handleReportCancel}
+//           okText="Yes"
+//           cancelText="No"
+//         >
+//           <Button type="default">
+//             <ExclamationCircleOutlined /> Report
+//           </Button>
+//         </Popconfirm>,
 //       ]}
 //       extra={
 //         <img
@@ -143,6 +115,7 @@ export default EventDetail;
 //       {post.description}
 //     </List.Item>
 //   );
+
 //   const handleLike = (post) => {
 //     setPosts((prevPosts) =>
 //       prevPosts.map((item) => {
@@ -157,44 +130,47 @@ export default EventDetail;
 //       })
 //     );
 //   };
+
 //   const handleReportCancel = (e) => {
 //     console.log(e);
-//     message.error('Click on No');
+//     message.error("Click on No");
 //   };
+
 //   const handleReportConfirm = (e) => {
 //     console.log(e);
-//     message.success('Thanks');
+//     message.success("Thanks");
 //     setIsReported(true);
 //   };
 
-// useEffect(() => {
-//   const fetchEvent = async () => {
-//     try {
-//       const response = await axios.get(`https://64890c550e2469c038fe9625.mockapi.io/VN_HS/event/${eventID}`);
-//       setEvent(response.data);
-//     } catch (error) {
-//       console.error("Error fetching event details:", error);
-//     }
-//   };
+//   useEffect(() => {
+//     const fetchEvent = async () => {
+//       try {
+//         const response = await axios.get(`https://64890c550e2469c038fe9625.mockapi.io/VN_HS/event/${eventID}`);
+//         console.log('Fetched eventID:', response.data.eventID);
+//         setEvent(response.data);
+//       } catch (error) {
+//         console.error("Error fetching event details:", error);
+//       }
+//     };
 
-//   const fetchPosts = async () => {
-//     try {
-//       const response = await axios.get("https://64890c550e2469c038fe9625.mockapi.io/VN_HS/post");
-//       const filteredPosts = response.data.filter((post) => post.eventID === eventID);
-//       setPosts(filteredPosts);
-//     } catch (error) {
-//       console.error("Error fetching posts:", error);
-//     }
-//   };
+//     const fetchPosts = async () => {
+//       try {
+//         const response = await axios.get("https://64890c550e2469c038fe9625.mockapi.io/VN_HS/post");
+//         const filteredPosts = response.data.filter((post) => post.eventID === eventID);
+//         setPosts(filteredPosts);
+//       } catch (error) {
+//         console.error("Error fetching posts:", error);
+//       }
+//     };
 
-//   fetchEvent();
-//   fetchPosts();
-// }, [eventID]);
+//     fetchEvent();
+//     fetchPosts();
+//   }, [eventID]);
 
 //   if (!event) {
 //     return <div>Loading...</div>;
 //   }
-
+// <CreatePost />
 //   return (
 //     <div className="event">
 //       <h2>Event Detail</h2>
@@ -203,21 +179,21 @@ export default EventDetail;
 //       <p>Description: {event.description}</p>
 
 //       <h2>Posts</h2>
-//       <div className="create-post">
+//       <EventContext.Provider value={event.eventID}>
 //       <CreatePost />
-//     </div>
-//     <List
-//       itemLayout="vertical"
-//       size="large"
-//       pagination={{
-//         onChange: (page) => {
-//           console.log(page);
-//         },
-//         pageSize: 10,
-//       }}
-//       dataSource={posts}
-//       renderItem={(post) => <PostItem post={post} />}
-//     />
+//     </EventContext.Provider>
+//       <List
+//         itemLayout="vertical"
+//         size="large"
+//         pagination={{
+//           onChange: (page) => {
+//             console.log(page);
+//           },
+//           pageSize: 10,
+//         }}
+//         dataSource={posts}
+//         renderItem={(post) => <PostItem post={post} />}
+//       />
 //     </div>
 //   );
 // };
