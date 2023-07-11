767fcaf2692ad370a2b4f107b3fe8b4b728d9d0a
deadline-backup <timerush123@gmail.com>
2023-07-11 15:27:41 +0700

    modified:   src/components/Profile/Profile.css 	modified:   src/components/Profile/Profile.js

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,10 +2,19 @@ import { Col, Row, Button } from "antd";
 import "./Profile.css";
 import ChangePass from "./ChangePass";
 import ToMentor from "./UpToMentor";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 const storedSession = JSON.parse(sessionStorage.getItem("session"));
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true); // Initially set loading state to true
   const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    // Simulating loading data from an API or performing some async operation
+    setTimeout(() => {
+      setIsLoading(false); // Set loading state to false after a certain delay
+    }, 2000); // Adjust the delay time according to your needs
+  }, []);
+  
   const handleOpen = () => {
     setIsOpen(true);
   };
@@ -23,108 +32,67 @@ const App = () => {
   };
   return (
     <>
-      <div className="profile" style={{ height: "760px" }}>
-        <h2
-          style={{
-            textAlign: "center",
-            fontSize: "50px",
-            paddingBottom: "20px",
-          }}
-        >
-          Trang cá nhân
-        </h2>
-        <Row
-          style={{
-            height: "254px",
-          }}
-        >
-          <Col span={12}>
-            <div className="">
-              <img
-                src="https://images.unsplash.com/photo-1585591424436-a55926755d91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
-                alt="..."
-                style={{
-                  height: "300px",
-                  position: "absolute",
-                  right: "50px",
-                  borderRadius: "10px",
-                  boxShadow: "-1px 1px 62px -18px rgba(0, 0, 0, 0.19)",
-                }}
-              />
-            </div>
-          </Col>
-          <Col span={12}>
-            <div>
-              <div>
-                <h3 className="h2 text-black mb-0">
-                  Tên người dùng: {storedSession.username}
-                </h3>
+      {isLoading ? (
+        <div className="spinner"></div>
+      ) : (
+        <div className="profile" style={{ height: "760px" }}>
+          <h2 style={{ textAlign: "center", fontSize: "50px", paddingBottom: "20px" }}>
+            Trang cá nhân
+          </h2>
+          <Row style={{ height: "254px" }}>
+            <Col span={12}>
+              <div className="">
+                <img
+                  src="https://images.unsplash.com/photo-1585591424436-a55926755d91?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                  alt="..."
+                  style={{
+                    height: "300px",
+                    position: "absolute",
+                    right: "50px",
+                    borderRadius: "10px",
+                    boxShadow: "-1px 1px 62px -18px rgba(0, 0, 0, 0.19)",
+                  }}
+                />
               </div>
-              <ul className="list-unstyled mb-1-9">
-                <li
-                  className=" h4 mb-2 mb-xl-3 display-28"
-                  style={{ fontFamily: "Archivo Narrow" }}
-                >
-                  <span className="display-26 text-secondary me-2 font-weight-600">
-                    Chức vụ:
-                  </span>{" "}
-                  {storedSession.role}
-                </li>
-                <li
-                  className=" h4 mb-2 mb-xl-3 display-28"
-                  style={{ fontFamily: "Archivo Narrow" }}
-                >
-                  <span className="display-26 text-secondary me-2 font-weight-600">
-                    ID:
-                  </span>{" "}
-                  {storedSession.id}
-                </li>
-                <li
-                  className="h4 mb-2 mb-xl-3 display-28"
-                  style={{ fontFamily: "Archivo Narrow" }}
-                >
-                  <div>
+            </Col>
+            <Col span={12}>
+              <div>
+                <div>
+                  <h3 className="h2 text-black mb-0">Tên người dùng: {storedSession.username}</h3>
+                </div>
+                <ul className="list-unstyled mb-1-9">
+                  <li className="h4 mb-2 mb-xl-3 display-28" style={{ fontFamily: "Archivo Narrow" }}>
+                    <span className="display-26 text-secondary me-2 font-weight-600">Chức vụ:</span>{" "}
+                    {storedSession.role}
+                  </li>
+                  <li className="h4 mb-2 mb-xl-3 display-28" style={{ fontFamily: "Archivo Narrow" }}>
+                    <span className="display-26 text-secondary me-2 font-weight-600">ID:</span>{" "}
+                    {storedSession.id}
+                  </li>
+                  <li className="h4 mb-2 mb-xl-3 display-28" style={{ fontFamily: "Archivo Narrow" }}>
                     {!isOpen && (
-                      <Button onClick={handleOpen} size="large">
-                        Đổi mật khẩu
+                      <Button type="primary" onClick={handleOpenn}>
+                        Upgrade to Mentor
                       </Button>
                     )}
-                    {isOpen && (
-                      <div>
-                        <Button onClick={handleClose} size="large">
-                          Hủy
-                        </Button>
-                        <ChangePass />
-                      </div>
-                    )}
-                  </div>
-                </li>
-                <li
-                  className="h4 mb-2 mb-xl-3 display-28"
-                  style={{ fontFamily: "Archivo Narrow" }}
-                >
-                  <div>
-                    {!isOpenn && (
-                      <Button onClick={handleOpenn} size="large">
-                        Đăng ký làm Mentor
-                      </Button>
-                    )}
-                    {isOpenn && (
-                      <div>
-                        <Button onClick={handleClosen} size="large">
-                          Hủy
-                        </Button>
-                        <ToMentor />
-                      </div>
-                    )}
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </Col>
-        </Row>
-      </div>
+  
+                    {isOpenn && <ToMentor handleClosen={handleClosen} />}
+                  </li>
+                </ul>
+  
+                <a href={storedSession.resetLink} onClick={handleOpen}>
+                  Thay đổi mật khẩu
+                </a>
+                {isOpen && <ChangePass handleClose={handleClose} />}
+              </div>
+            </Col>
+          </Row>
+        </div>
+      )}
     </>
   );
+  
+  
 };
+
 export default App;
