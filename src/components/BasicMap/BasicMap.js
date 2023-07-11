4b7ee138bc45a8f65dd7c068ee2c2ce9c98d0cfa
Anh Quan <qsao2212@gmail.com>
2023-07-11 22:58:24 +0700

    add marker of map

--- src/components/BasicMap/BasicMap.js
+++ src/components/BasicMap/BasicMap.js
@@ -1,28 +1,25 @@
-import React, { useState } from "react";
-
-// import marker from "../../assets/image/—Pngtree—location marker_5990782.png"
-import { TileLayer } from "react-leaflet";
-import osm from "../../utils/osm-providers";
-import { useRef } from "react";
+import React, { useState, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { MapContainer } from "react-leaflet";
+import osm from "../../utils/osm-providers";
+import markerIcon from "./Daco_1014210.png";
 import "./Map.css";
-// import L, { Marker, Popup } from "leaflet";
-// const makerIcon = new L.Icon({
-//     iconUrl: require({marker}),
-//     iconSize: [35,45],
-// });
+import { Link } from "react-router-dom";
 
 const BasicMap = () => {
   const [center, setCenter] = useState({ lat: 16.7451, lng: 107.1852 });
-
   const ZOOM_LEVEL = 5.5;
   const mapRef = useRef();
-  console.log(osm.maptiler.url);
+
+  const customMarkerIcon = new L.Icon({
+    iconUrl: markerIcon,
+    iconSize: [45, 45],
+  });
+
   return (
     <>
       <div page="leafletBasic" />
-
       <div className="row">
         <div className="col text-center">
           <h2>React-leaflet - Basic Openstreet Maps</h2>
@@ -33,13 +30,46 @@ const BasicMap = () => {
                 url={osm.maptiler.url}
                 attribution={osm.maptiler.attribution}
               />
-
-              {/* <Marker position = {[16.7451 , 107.1852 ]} 
-                            icon = {makerIcon}
-                            />
-                            <Popup>
-                                <b>hello</b>
-                            </Popup> */}
+              <Marker position={[16.0678, 108.2208]} icon={customMarkerIcon}>
+                <Popup>
+                  <Link
+                    to={"./events/eventDetail/81"}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <strong>Chiến sự ở Đà Nẵng</strong>
+                  </Link>
+                </Popup>
+              </Marker>
+              <Marker position={[10.7629, 106.682]} icon={customMarkerIcon}>
+                <Popup>
+                  <Link
+                    to={"./events/eventDetail/82"}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <strong>Chiến sự ở Gia Định</strong>
+                  </Link>
+                </Popup>
+              </Marker>
+              <Marker position={[21.3858, 103.0188]} icon={customMarkerIcon}>
+                <Popup>
+                  <Link
+                    to={"/events/eventDetail/20"}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <strong>Điện Biên Phủ trên không</strong>
+                  </Link>
+                </Popup>
+              </Marker>
+              <Marker position={[16.4637, 107.5909]} icon={customMarkerIcon}>
+                <Popup>
+                  <Link
+                    to={"./events/eventDetail/52"}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <strong>Cuộc phản công ở kinh thành Huế</strong>
+                  </Link>
+                </Popup>
+              </Marker>
             </MapContainer>
           </div>
         </div>
